4a0f911f178714b50fe63289a46f0537f7f483a8
410011max <410011max@gmail.com>
2024-02-29 12:20:15 +0000

    del ori scripts

--- model/smoothquant/scripts.sh
+++
@@ -1,6 +0,0 @@
-python model/generate_act_scales.py \
-    --model-name meta-llama/Llama-2-7b-hf \
-    --output-path act_scales/llama2-7b.pt \
-    --num-samples 512 \
-    --seq-len 4096 \
-    --dataset-path wikitext2
