c044198d3ff7dd774899b698a189ae50af5b3ea6
410011max <410011max@gmail.com>
2024-03-04 01:59:26 +0000

    add W4A4 scripts

--- scripts/scripts.sh
+++ scripts/scripts.sh
@@ -84,3 +84,13 @@ python model/main.py meta-llama/Llama-2-7b-hf wikitext2 \
     --a_clip_ratio 0.9 --w_clip_ratio 0.85 \
     --keeper 0 --keeper_precision 3 --use_gptq \
     --eval_ppl
+
+# Atom W4A4 in paper
+CUDA_VISIBLE_DEVICES=2 \
+python model/main.py meta-llama/Llama-2-7b-hf wikitext2 \
+    --wbits 4 --abits 4 --a_sym --w_sym \
+    --act_group_size 128 --weight_group_size 128 --weight_channel_group 2 \
+    --reorder --act_sort_metric hessian \
+    --a_clip_ratio 0.9 --w_clip_ratio 0.85 \
+    --keeper 128 --keeper_precision 3 --kv_cache --use_gptq \
+    --eval_ppl
