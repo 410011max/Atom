06cda6b3ab1b6f50e29a0aed638d0fd467bdef74
410011max <410011max@gmail.com>
2024-03-11 05:28:55 +0800

    update scripts

--- model/smoothquant/get_act_scales.sh
+++ model/smoothquant/get_act_scales.sh
@@ -1,6 +1,7 @@
 CUDA_VISIBLE_DEVICES=1 \
 python model/generate_act_scales.py \
     --model-name meta-llama/Llama-2-7b-hf \
-    --output-path act_scales/llama2-7b-hf.pt \
+    --mode smooth \
+    --output-path act_scales/llama2-7b-hf-smooth-wikitext2.pt \
     --num-samples 512 \
     --seq-len 4096
