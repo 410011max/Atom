1143dcd1082f8e5024710a139b48403c5a5510d5
410011max <410011max@gmail.com>
2024-03-11 15:00:46 +0800

    not to quant attn output

--- model/smoothquant/quant.py
+++ model/smoothquant/quant.py
@@ -6,6 +6,8 @@ def quantize_llama(model, weight_quant='per_tensor', act_quant='per_tensor', qua
     progress_bar = tqdm(list(model.named_modules()))
     for name, m in tqdm(list(model.named_modules())):
         progress_bar.set_description(f"Processing {name}")
+        if name == "model.layers.1.self_attn":
+            break
         if isinstance(m, LlamaMLP):
             m.gate_proj = W8A8Linear.from_float(m.gate_proj, weight_quant=weight_quant, act_quant=act_quant,
                                                 scales=scales[name + ".gate_proj"] if scales else None)
@@ -15,15 +17,12 @@ def quantize_llama(model, weight_quant='per_tensor', act_quant='per_tensor', qua
                                                 scales=scales[name + ".down_proj"] if scales else None)
         elif isinstance(m, LlamaAttention):
             # Her we simulate quantizing BMM inputs by quantizing the output of q_proj, k_proj, v_proj
-            m.q_proj = W8A8Linear.from_float(
-                m.q_proj, weight_quant=weight_quant, act_quant=act_quant, 
-                quantize_output=quantize_bmm_input, scales=scales[name + ".q_proj"] if scales else None)
-            m.k_proj = W8A8Linear.from_float(
-                m.k_proj, weight_quant=weight_quant, act_quant=act_quant, 
-                quantize_output=quantize_bmm_input, scales=scales[name + ".k_proj"] if scales else None)
-            m.v_proj = W8A8Linear.from_float(
-                m.v_proj, weight_quant=weight_quant, act_quant=act_quant, 
-                quantize_output=quantize_bmm_input, scales=scales[name + ".v_proj"] if scales else None)
+            m.q_proj = W8A8Linear.from_float(m.q_proj, weight_quant=weight_quant, act_quant=act_quant, 
+                                             scales=scales[name + ".q_proj"] if scales else None)
+            m.k_proj = W8A8Linear.from_float(m.k_proj, weight_quant=weight_quant, act_quant=act_quant, 
+                                             scales=scales[name + ".k_proj"] if scales else None)
+            m.v_proj = W8A8Linear.from_float(m.v_proj, weight_quant=weight_quant, act_quant=act_quant, 
+                                             scales=scales[name + ".v_proj"] if scales else None)
             m.o_proj = W8A8Linear.from_float(m.o_proj, weight_quant=weight_quant, act_quant=act_quant,
                                              scales=scales[name + ".o_proj"] if scales else None)
     return model
