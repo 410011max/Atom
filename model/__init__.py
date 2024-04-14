396012e38bea399d63281f2d23b136709db8092f
410011max <410011max@gmail.com>
2024-04-14 13:06:42 +0000

    add init code

---
+++ model/__init__.py
@@ -0,0 +1,6 @@
+import os
+import sys
+
+current_path = os.path.dirname(os.path.abspath(__file__))
+parent_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(current_path)
