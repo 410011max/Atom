9d92f34b3e239bd7c29cab19e4c347eaf824f2c5
410011max <410011max@gmail.com>
2024-04-28 07:06:55 +0000

    fix coding style

--- model/smoothquant/quant.py
+++ model/smoothquant/quant.py
@@ -21,7 +21,6 @@ def quantize_llama(model, weight_quant='per_tensor', act_quant='per_tensor',
                 m.down_proj = W8A8Linear.from_float(m.down_proj, weight_quant=weight_quant, act_quant=act_quant, quantize_output=quantize_output,
                                                     scales=scales[name + ".down_proj"] if scales else None)
         elif isinstance(m, LlamaAttention):
-            # Her we simulate quantizing BMM inputs by quantizing the output of q_proj, k_proj, v_proj
             m.q_proj = W8A8Linear.from_float(m.q_proj, weight_quant=weight_quant, act_quant=act_quant, quantize_output=quantize_output,
                                              scales=scales[name + ".q_proj"] if scales else None,
                                              a_clip_ratio=a_clip_ratio, w_clip_ratio=w_clip_ratio)
@@ -34,4 +33,5 @@ def quantize_llama(model, weight_quant='per_tensor', act_quant='per_tensor',
             m.o_proj = W8A8Linear.from_float(m.o_proj, weight_quant=weight_quant, act_quant=act_quant, quantize_output=quantize_output,
                                              scales=scales[name + ".o_proj"] if scales else None,
                                              a_clip_ratio=a_clip_ratio, w_clip_ratio=w_clip_ratio)
+
     return model
